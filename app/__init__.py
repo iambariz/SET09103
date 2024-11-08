65f862b0227a4ce6570b0ac1a7951459aaf3a58c
iambariz <david.barkanyi96@gmail.com>
2024-11-08 16:34:26 +0000

    Created basic route for recipes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 # app/__init__.py
 from flask import Flask
 from .api.internal_api import internal_api_bp  # Import the blueprint
+from .config import Config
+
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
     # Register the blueprint
     app.register_blueprint(internal_api_bp)
