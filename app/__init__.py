897371a809e6d0ca9ddb3d369ff68d0f52d17e52
iambariz <david.barkanyi96@gmail.com>
2024-11-09 17:30:03 +0000

    Navbar done + some changes in controllers

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,37 @@
 # app/__init__.py
+
 from flask import Flask, render_template
-from .api.internal_api import internal_api_bp  # Import the blueprint
+from .api.internal_api import internal_api_bp
+from .models import User
 from .config import Config
 from .extensions import db, migrate
 from .routes.auth import auth_bp
+from .routes.main import main_bp
+from flask_login import LoginManager
 
+# Define LoginManager globally
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)#
+    app.config.from_object(Config)
 
-    # Initialize extensions
+    # Initialize LoginManager
+    login_manager.init_app(app)
+
+    # Initialize other extensions
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register the blueprint
+    # Register Blueprints
     app.register_blueprint(internal_api_bp)
     app.register_blueprint(auth_bp)
-    
+    app.register_blueprint(main_bp)
+
     return app
+
+# Define user_loader callback outside `create_app`
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,7 @@
 from .auth import auth_bp
+from .main import main_bp
 
 def register_blueprints(app):
     # Register the user blueprint
     app.register_blueprint(auth_bp)
+    app.register_blueprint(main_bp)
